e0aad0b65ad703b6f278389e742a6efe42aaa3e0
Sbinb <1057015@hr.nl>
2023-04-25 15:12:22 +0200

    new code

--- src/js/game.js
+++ src/js/game.js
@@ -1,22 +1,47 @@
-import '../css/style.css'
-import { Actor, Engine, Vector } from "excalibur"
-import { Resources, ResourceLoader } from './resources.js'
+import { Actor, Engine, Vector, Label, Color, Font } from "excalibur";
+import { Resources, ResourceLoader } from "./resources.js";
 
 export class Game extends Engine {
+  constructor() {
+    super({ width: 640, height: 480 });
+    this.start(ResourceLoader).then(() => this.startGame());
+  }
 
-    constructor() {
-        super({ width: 800, height: 600 })
-        this.start(ResourceLoader).then(() => this.startGame())
-    }
+  startGame() {
+    const BG = new Actor();
+    BG.graphics.use(Resources.BG.toSprite());
+    BG.pos = new Vector(400, 300);
+    BG.vel = new Vector(0, 0);
+    BG.scale = new Vector (0.4, 0.4);
+    BG.enableCapturePointer = true;
+    BG.on("pointerup", (event) => {event.pos = new Vector(300,500) });   // verplaatsen
+    this.add(BG);
+  
+    const fish = new Actor();
+    fish.graphics.use(Resources.Fish.toSprite());
+    fish.pos = new Vector(400, 300);
+    fish.vel = new Vector(30, 10);
+    fish.scale = new Vector (0.5, 0.5);
+    fish.enableCapturePointer = true;
+    fish.on("pointerup", (event) => {event.pos = new Vector(300,500) });   // verplaatsen
+    this.add(fish);
 
-    startGame() {
-        console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(400, 300)
-        fish.vel = new Vector(-10,0)
-        this.add(fish)
-    }
+    const anotherFish = new Actor();
+    anotherFish.graphics.use(Resources.Fish2.toSprite());
+    anotherFish.pos = new Vector(100, 100);
+    anotherFish.vel = new Vector(10, 10);
+    anotherFish.scale = new Vector (1.5,1.5);
+    this.add(anotherFish);
+  
+    const thirdFish = new Actor();
+    thirdFish.graphics.use(Resources.Fish3.toSprite());
+    thirdFish.pos = new Vector(400, 200);
+    thirdFish.vel = new Vector(10, -10);
+    thirdFish.scale = new Vector (1,1);
+    this.add(thirdFish);
+  }
 }
 
-new Game()
+
+new Game();
+
--- src/js/resources.js
+++ src/js/resources.js
@@ -1,9 +1,16 @@
-import { ImageSource, Sound, Resource, Loader } from 'excalibur'
-import fishImage from '../images/fish.png'
+import { ImageSource, Sound, Resource, Loader } from "excalibur";
+import fishImage from "./Sprite1.png";
+import fish2Image from "./Sprite2.png";
+import fish3Image from "./Sprite3.png";
+import BGImage from "./BG.jpeg";
 
 const Resources = {
-    Fish: new ImageSource(fishImage)
-}
-const ResourceLoader = new Loader([Resources.Fish])
+  Fish: new ImageSource(fishImage),
+  Fish2: new ImageSource (fish2Image),
+  Fish3: new ImageSource (fish3Image),
+  BG: new ImageSource (BGImage)
+};
 
-export { Resources, ResourceLoader }
+const ResourceLoader = new Loader([Resources.Fish, Resources.Fish2, Resources.Fish3, Resources.BG]);
+
+export { Resources, ResourceLoader };
